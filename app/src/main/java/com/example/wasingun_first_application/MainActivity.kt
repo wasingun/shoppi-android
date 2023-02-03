7b28615255744975c276bda017975a7004b5607a
wasingun <wasingun00@gmail.com>
2023-02-04 02:17:45 +0900

    강의 48 스타일, 및 theme 적용

--- app/src/main/java/com/example/wasingun_first_application/MainActivity.kt
+++ app/src/main/java/com/example/wasingun_first_application/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.wasingun_first_application
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
     }
 }
