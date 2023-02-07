c5c297dd7f27df1de9ebf98ba412695778867376
wasingun <wasingun00@gmail.com>
2023-02-07 20:47:46 +0900

    강의 48 스타일, 및 theme 적용

--- app/src/main/java/com/example/wasingun_first_application/MainActivity.kt
+++ app/src/main/java/com/example/wasingun_first_application/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.wasingun_first_application
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val BottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        BottomNavigationView.itemIconTintList = null
     }
 }
