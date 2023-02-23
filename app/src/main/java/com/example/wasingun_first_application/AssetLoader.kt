581e6a2225d64be5b2669a0f4c35630ae1f73064
wasingun <wasingun00@gmail.com>
2023-02-23 23:50:39 +0900

    json 로드하는 과정 작성

--- app/src/main/java/com/example/wasingun_first_application/AssetLoader.kt
+++ app/src/main/java/com/example/wasingun_first_application/AssetLoader.kt
@@ -13,11 +13,10 @@ class AssetLoader {
 
     private fun loadAsset(context:Context, fileName: String): String {
         return context.assets.open(fileName).use {inputStream ->
-            val size = inputStream.available()
-            val bytes = ByteArray(size)
-            inputStream.read(bytes)
-            val homeData = String(bytes)
-            String(bytes)
+            val size = inputStream.available() // 바이트 수를 반환
+            val bytes = ByteArray(size) // 바이트 수 만큼의 버퍼를 반환
+            inputStream.read(bytes) //inputStream에서 bytes 만큼 크기를 불러들임
+            String(bytes) // bytes의 내용을 string으로 반환
         }
     }
 }
